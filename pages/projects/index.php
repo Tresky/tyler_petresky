115099827583faae216d9c925ccf27a8cf48d384
Tyler <tnpetresky@gmail.com>
2014-07-27 23:17:20 -0400

    Update content

--- pages/projects/index.php
+++ pages/projects/index.php
@@ -9,17 +9,19 @@ include($_SERVER['DOCUMENT_ROOT'] . '/templates/postheader.php');
 	<br>
 	<ul class="projects">
 		<li>
-			<h3 class="limbitless">Lim[b]itless Project</h3>
+			<h3 class="limbitless"><i>Limb</i>itless Project</h3>
 			<p>
-				The Lim[b]itless Project is an effort to provide a 6-year old boy
+				The <i>Limb</i>itless Project is an effort to provide a 6-year old boy
 				named Alex, who was born without a right arm, with an affordable,
 				fully electronic prosthetic limb. This project was born and taken on
 				by a group of students attending the University of Central Florida.
+				Alex, prviously without a right arm, now has a prosthetic right arm that
+				allows him to open and close his fingers and pick up objects.
 			</p>
 			<img src="../../assets/images/limbitless_pictures/limbitless_fam_team_1.jpg"
 				 alt="Lim[b]itless Family and the Team" width="75%">
-			<!--<img src="../../assets/images/limbitless_pictures/limbitless_team_1.jpg"
-				 alt="Lim[b]itless Family and the Team" width="75%">-->
+			<img src="../../assets/images/limbitless_pictures/alex_arm1.jpg"
+				 alt="Alex with his brand new arm." width="75%">
 		</li>
 		<li>
 			<h3 class="weatherstation">EWB Weatherstation</h3>
